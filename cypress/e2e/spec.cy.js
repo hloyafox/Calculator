2fbca727744fc67182e8dd6b1197d5e35ffda31d
Хлоя Стронг <hloyatrash@gmail.com>
2022-08-23 14:36:36 +0400

    no message

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://buildstats.volgaunion.ru/');
+    cy.visit('http://localhost:3000');
     cy.get('[data-cy="title"]').should('be.visible');
   })
 })
